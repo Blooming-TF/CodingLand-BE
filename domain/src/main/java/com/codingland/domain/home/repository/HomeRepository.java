1c1543b05c04c8a4c192e75ffb3e42c136770130
안정후 <austinan@naver.com>
2024-11-19 18:55:43 +0900

    feat: repository unused methods

--- domain/src/main/java/com/codingland/domain/home/repository/HomeRepository.java
+++ domain/src/main/java/com/codingland/domain/home/repository/HomeRepository.java
@@ -3,9 +3,5 @@ package com.codingland.domain.home.repository;
 import com.codingland.domain.home.entity.Home;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface HomeRepository extends JpaRepository<Home, Long> {
-    Optional<Home> getHome(Long homeId);
-    Optional<Home> editHome(Long homeId);
 }
